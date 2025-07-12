deb9dd57a1f0e830b2ccca79904649ce73f4da95
pociello <asierpociello@gmail.com>
2025-07-12 10:09:14 +0200

    Create GameBoard() and Box() factory functions

--- script.js
+++ script.js
@@ -6,11 +6,37 @@ function GameBoard() {
     for (let i=0; i<rows; i++) {
         board[i] = [];
         for (let j=0; j<columns; j++) {
-            board[i].push('')
+            board[i].push(Box())
         }
     }
 
-    console.log(board)
+    const getBoard = () => board;
+
+    const chooseBox = (row, column, player) => {
+        const box = board[row - 1][column - 1];
+        if (box.getValue() !== '') return;
+        box.addValue(player)
+    }
+    
+    const printBoard = () => {
+        const boardWithBoxValues = board.map(row => row.map(cell => cell.getValue()))
+        console.log(boardWithBoxValues)
+    }
+
+    return {getBoard, chooseBox, printBoard}
 }
 
-GameBoard()
+function Box() {
+    let value = '';
+
+    const addValue = (player) => {
+        value = player;
+    }
+
+    const getValue = () => value;
+
+    return {getValue, addValue}
+}
+
+const board = GameBoard();
+board.printBoard()
